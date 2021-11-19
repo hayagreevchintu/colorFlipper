0cc4f1def25198d859697ce124c7b141d2d084a2
Hayagreev <hayagreev.chintu1@gmail.com>
2021-11-19 20:18:08 +0530

    completed hex color flipper

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const colors = ["green", "red", "blue", "yellow"];
 
 colour = document.querySelector('.color');
 btn = document.querySelector('.btn');
--- hex.js
+++ hex.js
@@ -1,1 +1,16 @@
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+
+color = document.querySelector('.color');
+btn = document.querySelector('#btn');
+
+btn.addEventListener('click',()=>{
+    let hexColor = '#';
+    for(i=0; i<6; i++){
+        hexColor += hex[randomValue()];
+    }
+    document.body.style.backgroundColor = hexColor;
+    color.textContent = hexColor;
+});
+
+let randomValue = () => Math.floor(Math.random()*hex.length);
+
